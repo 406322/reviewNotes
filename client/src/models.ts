e2368ee65dcc6278380b9f2917227b47e8d3e951
Ludwig <ludwig.slettingdalen@gmail.com>
2022-10-11 13:50:49 +0200

    commit

---
+++ client/src/models.ts
@@ -0,0 +1,17 @@
+
+export interface ReviewNote {
+    assignees: any,
+    isClosed: boolean,
+    dueDate: string,
+    comments: any,
+    id: any,
+    title: string,
+    description: string,
+    sectionRef: string,
+    type: string,
+    status: string,
+    priority: any,
+    reporterId: any,
+    createdAt: any,
+    updatedAt: any
+}
