423a70bc094fecb65d34b9db47a1a8f33ff9d361
Ludwig <ludwig.slettingdalen@gmail.com>
2022-10-14 01:25:35 +0200

    table & header ok

--- client/src/components/table/table.tsx
+++ client/src/components/table/table.tsx
@@ -4,6 +4,7 @@ import { GrLink } from 'react-icons/gr'
 import { useAtom } from 'jotai'
 import { usersAtom, reviewNotesAtom, filteredReviewNotesAtom, filterStateAtom } from '../../atoms'
 import * as api from '../../api'
+import { formatDate, getPhoto, isDate } from './functions'
 
 
 export const Table = () => {
@@ -13,7 +14,6 @@ export const Table = () => {
     const [filteredReviewNotes, setFilteredReviewNotes] = useAtom(filteredReviewNotesAtom)
     const [filterState, setFilterState] = useAtom(filterStateAtom)
 
-
     useEffect(() => {
         api.getUsers()
             .then((result) => setUsers(result))
@@ -21,35 +21,11 @@ export const Table = () => {
             .then((result) => setReviewNotes(result))
     }, [])
 
-
     const handleLoadMore = () => {
         if (!reviewNotes) return
         const isMaxRows = (reviewNotes.length) <= filterState.rows
         if (isMaxRows) return
         setFilterState((prevState) => { return { ...prevState, rows: filterState.rows + 3 } })
-    }
-
-    const getPhoto = (element: string) => {
-        if (!users) return
-        const user = users.filter(user => user.id === element)
-        const photo = user[0].photo
-        return photo
-
-    }
-
-    const formatDate = (string: string) => {
-        const day = string.slice(8, 10)
-        const month = string.slice(5, 7)
-        const year = string.slice(0, 4)
-        return day + '.' + month + '.' + year
-    }
-
-    function isDate(dateString: any) {
-        if (typeof dateString !== 'string') return
-        var d = new Date(dateString);
-        var dNum = d.getTime();
-        if (!dNum && dNum !== 0) return false
-        return d.toISOString().slice(0, 10) === dateString;
     }
 
     const colNames = ["", "Title", "Type", "Status", "Priority", "Due date", "Assignees", "Reporter", "Section", "Created", "Updated"]
@@ -106,8 +82,8 @@ export const Table = () => {
                                         if (element === 'Medium') { return <td className='text-sm text-center text-white bg-yellow-500 border border-black' key={index}>{element}</td> }
                                         if (element === 'Low') { return <td className='text-sm text-center text-white bg-green-500 border border-black' key={index}>{element}</td> }
 
-                                        if (element === '001') { return <td key={index}>{<img className='w-8 h-8 rounded-full' src={getPhoto(element)} title={element} alt={element} />}</td> }
-                                        if (element === '002') { return <td key={index}>{<img className='w-8 h-8 rounded-full' src={getPhoto(element)} title={element} alt={element} />}</td> }
+                                        if (element === '001') { return <td key={index}>{<img className='w-8 h-8 rounded-full' src={getPhoto(users, element)} title={element} alt={element} />}</td> }
+                                        if (element === '002') { return <td key={index}>{<img className='w-8 h-8 rounded-full' src={getPhoto(users, element)} title={element} alt={element} />}</td> }
 
                                         if (isDate(element)) { return <td className='text-sm' key={uuidv4()}>{formatDate(element)}</td> }
 
--- client/src/pages/ReviewNotes.tsx
+++ client/src/pages/ReviewNotes.tsx
@@ -1,30 +1,12 @@
-import { Table } from "../components/table/table"
+import { Header } from "../components/header/header"
 import { Filter } from "../components/filter/filter"
+import { Table } from "../components/table/table"
+
 
 const ReviewNotes = () => {
     return (
         <div className="min-w-[1400px]">
-
-            <div className="flex gap-1 m-3">
-                <p className="text-sm text-blue-400 underline">Engagements</p>
-                <p className="text-sm">&gt;</p>
-                <p className="text-sm text-blue-400 underline">Microsoft 2021</p>
-                <p className="text-sm">&gt;</p>
-                <p className="text-sm">Review notes</p>
-            </div>
-
-            <div
-                className="flex gap-8 border-b-2 border-black">
-                <h1 className="p-5 text-3xl font-bold">Review notes</h1>
-                <div className="flex flex-col justify-center">
-                    <button
-                        className="w-16 border-2 border-black"
-                        onClick={() => alert('Here will be the form to create a new review note')}>
-                        New
-                    </button>
-                </div>
-            </div>
-
+            <Header />
             <Filter />
             <Table />
         </div>
